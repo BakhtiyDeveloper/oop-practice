6d93f6c4b6ff303b757d73f878506dd237683e0b
Bakhtiyer <bakhtiyer.kurbanov@bk.ru>
2021-07-21 07:07:36 +0500

    Add project files.

---
+++ PracticeOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PracticeOOP; 
+
+namespace PracticeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mynamePro = "Bahtiyor";
+            int myyearPro = 34;
+            
+
+            StringTransformer myname = new StringTransformer(mynamePro, myyearPro);
+            
+            myname.SayHi(); 
+             
+             
+            
+        
+        }
+    }
+}
---
+++ PracticeOOP/StringTransformer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeOOP
+{
+    class StringTransformer  /*Make a function that will return a greeting statement that uses an input;
+                              your program should return, "Hello, <name> how are you doing today?".*/  
+    {
+        private string name;
+        private int year;
+ 
+    
+        public StringTransformer (string nameOne, int yearOne) 
+        {
+            name = nameOne;
+            year = yearOne; 
+                
+        } 
+ 
+        public void SayHi() 
+        {
+            Console.WriteLine($"Hi {name}");    
+        
+        }   
+    
+    
+    
+    
+    
+    
+    }   
+}
+
+
+
+
+
+
+
+    
+            
+    
+    
+
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+
+
+
+     
+
+
+
+
+
+
